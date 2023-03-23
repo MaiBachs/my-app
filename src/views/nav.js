fabc152fe16211be786c09d9f66d5fbfe154e90f
MaiBachs <maibach699.com>
2023-03-23 21:00:34 +0700

    complete

---
+++ src/views/nav.js
@@ -0,0 +1,16 @@
+import "./nav.scss"
+
+const Nav = ()=>{
+    return(
+        <div>
+            <div class="topnav">
+            <a class="active" href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+      </div>
+        </div>
+    );
+}
+
+export default Nav;
